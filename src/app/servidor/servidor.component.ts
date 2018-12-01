0e0ef732ebbd6cae1ecc3d50d0973fef51c5aa82
emeery <gerardoemery@live.com.mx>
2018-12-01 03:47:05 -0600

    basicos a

--- src/app/servidor/servidor.component.ts
+++ src/app/servidor/servidor.component.ts
@@ -11,7 +11,8 @@ export class ServidorComponent {
     constructor() { }
     onVisible() {
         this.visible = !this.visible;
-        this.log.push(this.log.length + 1);
+        // this.log.push(this.log.length + 1);
+        this.log.push(new Date());
     }
 }
 
